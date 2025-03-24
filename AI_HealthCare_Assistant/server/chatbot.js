6c902a9477212cd93452b83741155ff74d4f5a68
NikhilKumar32 <168549751+NikhilKumar32@users.noreply.github.com>
2025-03-24 22:58:13 +0530

    Update chatbot.js

--- AI_HealthCare_Assistant/server/chatbot.js
+++ AI_HealthCare_Assistant/server/chatbot.js
@@ -1,6 +1,5 @@
 const dialogflow = require('dialogflow')
 const dialogflowConfig = require('./config/config')
-
 const projectId = dialogflowConfig.project_id
 const configuration = {
   credentials: {
@@ -8,13 +7,10 @@ const configuration = {
     client_email: dialogflowConfig.client_email,
   },
 }
-
 const sessionId = '997753'
 const languageCode = 'en-US'
 const sessionClient = new dialogflow.SessionsClient(configuration)
-
 const sessionPath = sessionClient.sessionPath(projectId, sessionId)
-
 const talkToChatbot = async (message) => {
   console.log('message ' + message)
   const botRequest = {
@@ -26,12 +22,10 @@ const talkToChatbot = async (message) => {
       },
     },
   }
-
   let response = await sessionClient
     .detectIntent(botRequest)
     .then((responses) => {
       //console.log(JSON.stringify(responses))
-
       const requiredResponse = responses[0].queryResult
       // console.log(requiredResponse)
       return requiredResponse
@@ -39,8 +33,6 @@ const talkToChatbot = async (message) => {
     .catch((error) => {
       console.log('ERROR: ' + error)
     })
-
   return response
 }
-
 module.exports = talkToChatbot
